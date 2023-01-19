f12c8fd5f6fc16ac73be4db0ddc1430e6ca31d3f
Bakarseck <seck.bakar@ugb.edu.edu.sn>
2023-01-19 23:33:28 +0000

    CrupPython

---
+++ crud.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# Define a variable mysqldb for connection
+mysqldb = mysql.connector.connect(host="localhost", user="root", password="",database="crudPython")
+
+# Connect for the cursor for the database
+mysqlcursor = mysqldb.cursor()
+
+# create a table
+#mysqlcursor.execute("create table studentrecord(rollno INT, name varchar(30), marks int)")
+# insert into table
+
+mysqlcursor.execute("insert into studentrecord(rollno, name, marks) values(1, 'Bakar', 20)")
+print("Record inserted into the table")
+mysqldb.commit()
+
+mysqldb.close()
