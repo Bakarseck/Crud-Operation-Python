29bc7bef9af46afc17089c6b9ba73c2f8393e914
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-01-25 10:44:49 +0000

    crud

--- crud.py
+++ crud.py
@@ -10,8 +10,8 @@ mysqlcursor = mysqldb.cursor()
 
 # create a table
 #mysqlcursor.execute("create table studentrecord(rollno INT, name varchar(30), marks int)")
-# insert into table
 
+# insert into table
 mysqlcursor.execute("insert into studentrecord(rollno, name, marks) values(1, 'Bakar', 20)")
 print("Record inserted into the table")
 mysqldb.commit()
